0ca3d56e6386f70f710edaf67571d106bd5e35ac
barnabemonnot <b@barnabemonnot.com>
2020-10-07 12:58:02 +0800

    Add notes to 1559 presentation

--- README.md
+++ README.md
@@ -48,6 +48,7 @@ Meet the RIG and check our previous talks!
 
 | Talk | Location | Date | Link |
 |---|---|---|---|
+| Ethereum's new transaction fee market design, EIP 1559 | Online (SUTD) | 25 September 2020 | [Notes/slides](assets/pdf/notes-georgios.pdf) |
 | cadCAD community call #2 | Online | 28 August 2020 | [Video](https://www.youtube.com/watch?v=SVNNvTiIKlg) + [Slides](https://docs.google.com/presentation/d/13PHT4fIw7g_fhOfkagZRyxjeVO8dM3z6zjRMQkY0j1U/edit?usp=sharing) |
 | Eth Magicians EIP 1559 panel | Online | 29 July 2020 | [Video](https://www.youtube.com/watch?v=kNSpugOwQ1o) |
 | EthCC[3] | Paris (remote from Singapore) | 3 March 2020 | [Slides](assets/pdf/rig-ethcc.pdf) |
--- ethdata/extract.sh
+++ ethdata/extract.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
-startblock=10740001
-endblock=10740005
+startblock=10850000
+endblock=10900000
 
 ethereumetl export_blocks_and_transactions --start-block $startblock --end-block $endblock \
 --provider-uri http://fullnode.dappnode:8545 --blocks-output blocks.csv --transactions-output transactions.csv
@@ -11,8 +11,13 @@ ethereumetl extract_csv_column --input transactions.csv --column hash --output t
 ethereumetl export_receipts_and_logs --transaction-hashes transaction_hashes.txt \
 --provider-uri http://fullnode.dappnode:8545 --receipts-output receipts.csv
 
-head receipts.csv # check that i am selecting the correct columns
+cut -d , -f 1,13,15,16,17 blocks.csv > blocks-cut.csv
+cut -d , -f 1,6 receipts.csv > receipts-cut.csv
+cut -d , -f 1,2,4,8,9,10 transactions.csv > transactions-cut.csv
 
-# cut -d , -f 1,13,15,16,17 blocks.csv > blocks-cut.csv
-# cut -d , -f 1,6 receipts.csv > receipts-cut.csv
-# cut -d , -f 1,2,4,8,9,10 transactions.csv > transactions-cut.csv
+rm blocks.csv
+rm receipts.csv
+rm transactions.csv
+mv transactions-cut.csv data/txs-$startblock-$endblock.csv
+mv blocks-cut.csv data/bxs-$startblock-$endblock.csv
+mv receipts-cut.csv data/rxs-$startblock-$endblock.csv
