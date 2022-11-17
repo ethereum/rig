0e7dca6ebe9313229b26349dafc3823d1590d462
Sam Richards <sbrichards@gmail.com>
2022-11-17 14:09:04 -0800

    Update homepage to RIG copy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,21 +8,20 @@ const Home: NextPage = () => {
     <>
       <PageMetadata
         title='Home'
-        description='Cryptography research group at the Ethereum Foundation.'
+        description='Robust Incentives Group at the Ethereum Foundation.'
       />
 
       <main>
         <Heading as='h1' mb={10}>
-          Cryptography Research at the Ethereum Foundation
+          Robust Incentives Group at the Ethereum Foundation
         </Heading>
 
         <Text>
-          The Ethereum Foundation leads research into cryptographic protocols that are useful within
-          the greater Ethereum community and more generally. Cryptography is a key tool that enables
-          greater functionality, security, efficiency, and auditability in decentralized settings.
-          We are currently conducting research into verifiable delay functions, multiparty
-          computation, vector commitments, and zero-knowledge proofs etc. We have a culture of open
-          source and no patents are put on any work that we produce.
+          The Robust Incentives Group is a research team of the Ethereum Foundation. We specialise
+          in incentive analysis for protocols, using methods from game theory, mechanism design,
+          empirical analysis and simulations. Since our foundation, we actively participated in
+          research on EIP-1559 and Proof-of-Stake Ethereum. Find our releases, posts, and papers on
+          this website!
         </Text>
       </main>
     </>
